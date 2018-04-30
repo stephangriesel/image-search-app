12f2b2afc8b6e73f01d1a53fba8de42be840e4a0
Stephan Griesel <sgriesel@gmail.com>
2018-04-30 14:45:37 +0200

    add textchange function

--- image_search/src/components/search/Search.js
+++ image_search/src/components/search/Search.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import TextField from 'material-ui/TextField';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
+import axios from 'axios';
 
 
 class Search extends Component {
@@ -11,14 +12,24 @@ class Search extends Component {
         apiUrl: 'https://pixabay.com/api',
         apiKey: '8802915-7204cb3cfd2b4d1caadaf9097',
         images: []
-    }
+    };
+
+    onTextChange = e => {
+        this.setState({ [e.target.name]: e.target.value }, () => {
+        axios.get(`${this.state.apiUrl}/?key=${this.state.apiKey}&q=${this.state.searchText}&image_type=photo&per_page=${this.state.amount}&safesearch=true`)
+          .then(res => this.setState({images: res.data.hits})) // promise 
+          .catch(err => console.log(err));
+    });
+    };
+
   render() {
+      console.log(this.state.images);
     return (
       <div>
         <TextField
             name="searchText"
             value={this.state.searchText}
-            onchange={this.onTextChange}
+            onChange={this.onTextChange}
             floatingLabelText="Search For Images"
             fullWidth={true}
         />
