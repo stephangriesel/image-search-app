f00886cc8d4938c0854e7c011675db2d5872df60
Stephan Griesel <sgriesel@gmail.com>
2018-04-30 15:57:48 +0200

    search funtionality working

--- image_search/src/components/search/Search.js
+++ image_search/src/components/search/Search.js
@@ -3,6 +3,7 @@ import TextField from 'material-ui/TextField';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
 import axios from 'axios';
+import ImageResults from '../image-results/ImageResults';
 
 
 class Search extends Component {
@@ -15,43 +16,56 @@ class Search extends Component {
     };
 
     onTextChange = e => {
-        this.setState({ [e.target.name]: e.target.value }, () => {
-        axios.get(`${this.state.apiUrl}/?key=${this.state.apiKey}&q=${this.state.searchText}&image_type=photo&per_page=${this.state.amount}&safesearch=true`)
-          .then(res => this.setState({images: res.data.hits})) // promise 
-          .catch(err => console.log(err));
-    });
-    };
+        const val = e.target.value;
+        this.setState({ [e.target.name]: val }, () => {
+          if (val === '') {
+            this.setState({ images: [] });
+          } else {
+            axios
+              .get(
+                `${this.state.apiUrl}/?key=${this.state.apiKey}&q=${
+                  this.state.searchText
+                }&image_type=photo&per_page=${this.state.amount}&safesearch=false`
+              )
+              .then(res => this.setState({ images: res.data.hits }))
+              .catch(err => console.log(err));
+          }
+        });
+      };
 
-    onAmountChange = (e,index,value) => this.setState({ amount:value });
+      onAmountChange = (e, index, value) => this.setState({ amount: value });
 
-  render() {
-      console.log(this.state.images);
-    return (
-      <div>
-        <TextField
-            name="searchText"
-            value={this.state.searchText}
-            onChange={this.onTextChange}
-            floatingLabelText="Search For Images"
-            fullWidth={true}
-        />
-        <br />
-        <SelectField
-          name="amount"
-          floatingLabelText="Amount"
-          value={this.state.amount}
-          onChange={this.onAmountChange}
-        >
-          <MenuItem value={5} primaryText="5" />
-          <MenuItem value={10} primaryText="10" />
-          <MenuItem value={15} primaryText="15" />
-          <MenuItem value={30} primaryText="30" />
-          <MenuItem value={50} primaryText="50" />
-        </SelectField>
-        <br />
-      </div>
-    )
-  }
-}
-
-export default Search;
+      render() {
+        console.log(this.state.images);
+        return (
+          <div>
+            <TextField
+              name="searchText"
+              value={this.state.searchText}
+              onChange={this.onTextChange}
+              floatingLabelText="Search For Images"
+              fullWidth={true}
+            />
+            <br />
+            <SelectField
+              name="amount"
+              floatingLabelText="Amount"
+              value={this.state.amount}
+              onChange={this.onAmountChange}
+            >
+              <MenuItem value={5} primaryText="5" />
+              <MenuItem value={10} primaryText="10" />
+              <MenuItem value={15} primaryText="15" />
+              <MenuItem value={30} primaryText="30" />
+              <MenuItem value={50} primaryText="50" />
+            </SelectField>
+            <br />
+            {this.state.images.length > 0 ? (
+              <ImageResults images={this.state.images} />
+            ) : null}
+          </div>
+        );
+      }
+    }
+    
+    export default Search;
