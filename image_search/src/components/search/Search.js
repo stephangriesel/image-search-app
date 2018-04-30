62cc1f0a27bdfbc1968c03763a153981a96d5373
Stephan Griesel <sgriesel@gmail.com>
2018-04-30 14:56:27 +0200

    add onAmountChange function

--- image_search/src/components/search/Search.js
+++ image_search/src/components/search/Search.js
@@ -22,6 +22,8 @@ class Search extends Component {
     });
     };
 
+    onAmountChange = (e,index,value) => this.setState({ amount:value });
+
   render() {
       console.log(this.state.images);
     return (
